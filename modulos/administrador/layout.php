0e1cf5d7aee80948d0cf11558687f33031a8ed04
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-10-28 13:35:14 -0500

    Cambio de conrenedor al display flex
    invertir controles de nav

--- modulos/administrador/layout.php
+++ modulos/administrador/layout.php
@@ -632,16 +632,17 @@
       <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard-pro/pages/dashboards/default.html " target="_blank">
         <img src="./img/logo-kuali.png" class="navbar-brand-img h-100" alt="main_logo">
       </a>
+
     </div>
     <hr class="horizontal dark mt-0">
     <div class="collapse navbar-collapse  w-auto h-auto" id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a id="btn_cargar_comp" class="nav-link" >
+          <a id="btn_cargar_comp" class="nav-link text-dark" >
             <div class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center">
               <i class="ni ni-shop text-primary text-sm opacity-10"></i>
             </div>
-            <span class="nav-link-text ms-1">Cargar comprobante</span>
+            <span class="nav-link-text ms-1 ">Cargar comprobante</span>
           </a>
         </li>
 
@@ -649,22 +650,29 @@
     </div>
 
   </aside>
+
+
+
   <main class="main-content position-relative border-radius-lg ">
     <!-- Navbar -->
     <nav class="navbar navbar-main navbar-expand-lg  px-0 mx-4 shadow-none border-radius-xl z-index-sticky " id="navbarBlur" data-scroll="false">
       <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-            <li class="breadcrumb-item text-sm">
-              <a class="text-white" href="javascript:;">
-                <i class="ni ni-box-2"></i>
+
+          <ul class="navbar-nav  justify-content-end">
+            <li class="nav-item d-flex align-items-center">
+              <a href="pages/authentication/signin/illustration.html" class="nav-link text-white font-weight-bold px-0" target="_blank">
+                <i class="fa fa-user me-sm-1"></i>
+                <span class="d-sm-inline d-none">Sign In</span>
               </a>
             </li>
-            <li class="breadcrumb-item text-sm text-white"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
-            <li class="breadcrumb-item text-sm text-white " aria-current="page">Default</li>
-          </ol>
-          <h6 class="font-weight-bolder mb-0 text-white">Default</h6>
-        </nav>
+          </ul>
+
+
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+
+          </div>
+
         <div class="sidenav-toggler sidenav-toggler-inner d-xl-block">
           <a href="javascript:;" class="nav-link p-0">
             <div class="sidenav-toggler-inner">
@@ -674,20 +682,9 @@
             </div>
           </a>
         </div>
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-          <div class="ms-md-auto pe-md-3 d-flex align-items-center">
 
-          </div>
-          <ul class="navbar-nav  justify-content-end">
-            <li class="nav-item d-flex align-items-center">
-              <a href="pages/authentication/signin/illustration.html" class="nav-link text-white font-weight-bold px-0" target="_blank">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sign In</span>
-              </a>
-            </li>
-
-          </ul>
         </div>
+
       </div>
     </nav>
     <!-- End Navbar -->
